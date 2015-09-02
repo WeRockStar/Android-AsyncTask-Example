0e1e5d499037ed0d6576559bac21b38d1fbb7eba
WeRockStar <vr_freestyle@outlook.com>
2015-09-02 15:49:30 +0700

    Create and Implement class for download

--- app/src/main/java/com/example/kotchaphan/asynctask/MainActivity.java
+++ app/src/main/java/com/example/kotchaphan/asynctask/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.kotchaphan.asynctask;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Environment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -27,13 +30,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        btnMusicPlay = (Button)findViewById(R.id.btnProgress);
+        btnMusicPlay = (Button) findViewById(R.id.btnProgress);
         btnMusicPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btnMusicPlay.setEnabled(false);
-                File file = new File(Environment.getExternalStorageDirectory().getPath()+"/jai_ho.mp3");
+                File file = new File(Environment.getExternalStorageDirectory().getPath() + "/jai_ho.mp3");
+
+                //check if file already exist
+                if (file.exists()) {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Warning...").setMessage("File already exsits").setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    builder.show();
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Warning...").setMessage("File doesn't exsits").setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+                    builder.show();
+                    //call Async Task onPreExcute state
+                    new DownloadMusic().execute(url);
+                }
             }
         });
+    }
+
+    class DownloadMusic extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
